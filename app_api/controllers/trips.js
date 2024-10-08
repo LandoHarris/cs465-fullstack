ebf034bd7bb277b892cf5ac98225a88497f1c870
Landon Harris <landonharris979@gmail.com>
2024-10-07 21:15:53 -0500

    Module 6 completed baseline

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -1,45 +1,104 @@
 const mongoose = require('mongoose');
-const Trip = require('../models/travlr'); //register model
+const Trip = require('../models/travlr'); // Register model
 const Model = mongoose.model('trips');
 
-//Get: /trips - list of all trips
-//Regardless of outcome, response must include HTML status code
-//and JSON message to the requesting client
+// Get: /trips - list of all trips
 const tripsList = async (req, res) => {
-    const q = await Model.find({}) //Return single record
-        .exec();
-
-        //uncomment the following line to show the results of the query
-        //on the console
-        //console.log(q);
-
-    if(!q)
-    { //database returned no data
-        return res
-                .status(404)    
-                .json(err);
-    } else {//return resulting trip list
-        return res  
-            .status(200)
-            .json(q);
+    try {
+        const trips = await Model.find({}).exec();
+        
+        if (!trips || trips.length === 0) {
+            return res.status(404).json({ message: 'No trips found' });
+        } else {
+            return res.status(200).json(trips);
+        }
+    } catch (err) {
+        console.error("Error retrieving trips:", err);
+        return res.status(500).json({ error: "Internal server error" });
     }
 };
+
+// Get: /trips/:tripCode - Find trip by code
 const tripsFindByCode = async (req, res) => {
-    const q = await Model.find({ code: req.params.tripCode }).exec();
-  
-    // console.log(q);
-  
-    if (!q) {
-      // Database returned no data
-      return res.status(404).json(err);
-    } else {
-      // Return resulting trip list
-      return res.status(200).json(q);
+    try {
+        const trip = await Model.findOne({ code: req.params.tripCode }).exec();
+        
+        if (!trip) {
+            return res.status(404).json({ message: 'Trip not found' });
+        } else {
+            return res.status(200).json(trip);
+        }
+    } catch (err) {
+        console.error("Error finding trip:", err);
+        return res.status(500).json({ error: "Internal server error" });
     }
-  };
-  
+};
+
+// POST: /trips - Add a new Trip
+const tripsAddTrip = async (req, res) => {
+    const newTrip = new Trip({
+        code: req.body.code,
+        name: req.body.name,
+        length: req.body.length,
+        start: req.body.start,
+        resort: req.body.resort,
+        perPerson: req.body.perPerson,
+        image: req.body.image,
+        description: req.body.description,
+    });
+
+    try {
+        const savedTrip = await newTrip.save();
+        return res.status(201).json(savedTrip);
+    } catch (err) {
+        console.error("Error adding trip:", err);
+        return res.status(400).json({ error: 'Failed to add trip' });
+    }
+};
+
+
+
+// PUT: /trips/:tripCode - Adds a new Trip
+// Regardless of outcome, response must include HTML status code
+// and JSON message to the requesting client
+const tripsUpdateTrip = async(req, res) => {
+// Uncomment for debugging
+console.log(req.params);
+console.log(req.body);
+const q = await Model
+.findOneAndUpdate(
+{ 'code' : req.params.tripCode },
+{
+code: req.body.code,
+name: req.body.name,
+length: req.body.length,
+start: req.body.start,
+resort: req.body.resort,
+perPerson: req.body.perPerson,
+image: req.body.image,
+description: req.body.description
+}
+)
+.exec();
+if(!q)
+{ // Database returned no data
+  return res
+    .status(400)
+    .json(err);
+} else { // Return resulting updated trip
+  return res
+    .status(201)
+    .json(q);
+}
+// Uncomment the following line to show results of
+operation
+// on the console
+// console.log(q);
+};
 
 module.exports = {
     tripsFindByCode,
-    tripsList
+    tripsList,
+    tripsAddTrip,
+    tripsUpdateTrip
 };
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,16 +1,17 @@
-const express = require('express'); //Express app
-const router = express.Router(); //router logic
 
-// This is where we import the controllers we will route
+const express = require("express");
+const router = express.Router();
+
 const tripsController = require("../controllers/trips");
 
-//define route for our trips endpoint
-router 
-    .route("/trips")
-    .get(tripsController.tripsList) //GET Method routes tripList
-//GET Method routes tripsFindByCode - requires parameter
 router
-    .route("/trips/:tripCode")
-    .get(tripsController.tripsFindByCode)   
+  .route("/trips")
+  .get(tripsController.tripsList)
+  .post(tripsController.tripsAddTrip);
+
+router
+  .route("/trips/:tripCode")
+  .get(tripsController.tripsFindByCode)
+  .put(tripsController.tripsUpdateTrip);
 
 module.exports = router;
